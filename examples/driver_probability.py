0c72d25d795c286b23e35b6d0cbb805796dfb27f
Angel Ramos <modesto.ramoshernand@ucalgary.ca>
2024-02-23 18:11:27 -0700

    driver_probability started

--- examples/driver_probability.py
+++ examples/driver_probability.py
@@ -1,4 +1,15 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from goph420_lab01 import integration as Itg
 
+# Gauss-Legendre quadrature method:
+dt = 0.04
+x = np.arange(0.0, 10 + dt, dt)
+mu = 0
+sigma = 1
+A = Itg.GaussDist(mu, sigma)
+lims = [0, 1]
+npts = 3
 
-
-if __name__ == "__main__":
+print(x)
+fx = Itg.integration_gauss(A, lims, npts)
