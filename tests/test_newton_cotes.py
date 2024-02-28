e7fabd3b3f265521e583cc3cf7a2333b1ba65733
Angel Ramos <modesto.ramoshernand@ucalgary.ca>
2024-02-28 11:06:08 -0700

    tests updated according changes on drivers and integration

--- tests/test_newton_cotes.py
+++ tests/test_newton_cotes.py
@@ -24,33 +24,17 @@ class TestNewton(unittest.TestCase):
     def test_simp13_integral(self):
         x = np.arange(0, np.pi/2, np.pi/1000)
         fx = np.cos(x)*np.sin(x)
-        result = Itg.integrate_newton(x, fx, 'simp1/3')
+        result = Itg.integrate_newton(x, fx, 'simp')
         self.assertAlmostEqual(result, 0.5, places=4)
 
         x = np.arange(0, 2, 0.000001)
         fx = (x + 2)
-        result = Itg.integrate_newton(x, fx, 'simp1/3')
+        result = Itg.integrate_newton(x, fx, 'simp')
         self.assertAlmostEqual(result, 6, places=4)
 
         x = np.arange(-1, 1, 0.000001)
         fx = (3*x**3 - x**2 + x - 1)
-        result = Itg.integrate_newton(x, fx, 'simp1/3')
-        self.assertAlmostEqual(result, -8/3, places=4)
-
-    def test_simp38_integral(self):
-        x = np.arange(0, np.pi/2, np.pi/1000)
-        fx = np.cos(x)*np.sin(x)
-        result = Itg.integrate_newton(x, fx, 'simp3/8')
-        self.assertAlmostEqual(result, 0.5, places=4)
-
-        x = np.arange(0, 2, 0.000001)
-        fx = (x + 2)
-        result = Itg.integrate_newton(x, fx, 'simp3/8')
-        self.assertAlmostEqual(result, 6, places=4)
-
-        x = np.arange(-1, 1, 0.000001)
-        fx = (3*x**3 - x**2 + x - 1)
-        result = Itg.integrate_newton(x, fx, 'simp3/8')
+        result = Itg.integrate_newton(x, fx, 'simp')
         self.assertAlmostEqual(result, -8/3, places=4)
 
     def test_raises_newton(self):
